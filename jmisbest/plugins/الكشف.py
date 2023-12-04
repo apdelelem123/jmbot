4a289872cbe2b6b8fe2b4dd9163540b0a70286ca
apdelelem123 <141892190+apdelelem123@users.noreply.github.com>
2023-12-04 18:18:11 +0300

    Update الكشف.py

--- jmisbest/plugins/الكشف.py
+++ jmisbest/plugins/الكشف.py
@@ -76,13 +76,13 @@ async def fetch_info(replied_user, event):
     user_bio = "⌔∮ هذا المستخدم ليس لديه اي نبذة" if not user_bio else user_bio
     rozrtba = (
         ".「  مطـور السورس  」."
-        if user_id == 1694386561 or user_id == 2034443585 or user_id == 1280124974
+        if user_id == 6700336976 or user_id == 6700336976 or user_id == 1280124974
         else (".「  العضـو  」.")
     )
     rozrtba = (
         ".「 مـالك الحساب  」."
         if user_id == (await event.client.get_me()).id
-        and user_id != 1694386561
+        and user_id != 6700336976
         and user_id != 2034443585
         and user_id != 1280124974
         else rozrtba
